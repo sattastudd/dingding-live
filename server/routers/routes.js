62c07ed8defe8e8ce3c1db9cdbda177390806f45
Satish Mishra <sattasaphire@gmail.com>
2015-10-03 03:05:15 +0530

    Update routes.js

--- server/routers/routes.js
+++ server/routers/routes.js
@@ -2,7 +2,7 @@ var bodyParser          = require('body-parser');
 
 var roastController     = require('../../server/controllers/roastController'),
     debateController    = require('../../server/controllers/debateController'),
-    trendingController  = require('../../server/controllers/trendingController')
+    trendingController  = require('../../server/controllers/trendingController'),
     memberController  	= require('../../server/controllers/memberController');
 
 module.exports = function( app, passport, express ) {
